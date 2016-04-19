a3ab20a0c1160bbea558686c850b8ac71a192af7
red1000 <red1.derouich@gmail.com>
2016-04-19 14:31:07 +0200

    comment 4

--- TestWebservicerestfull/src/service/CtoFService.java
+++ TestWebservicerestfull/src/service/CtoFService.java
@@ -14,7 +14,7 @@ public class CtoFService {
         Double fahrenheit;
         Double celsius = 36.8;
         fahrenheit = ((celsius * 9) / 5) + 32;
-        // this is my comment 3
+        // this is my comment 4
         String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
         return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
     }
